f85c6144c6ee1dbbfa5d3d31675cce781ac3cddb
Robert Byrne <mr.rob.byrne@gmail.com>
2016-07-28 23:01:59 -0230

    Adding a name variable on project generation yo
    This commit adds to the indes.js that is ran by using the command…
    
    `yo rb00 <nameOfProject>`
    
    It then prints out the name and some random string. The random string is there because a minimun of 1 method was needed to remove the warnings.

--- generator-rb00/app/index.js
+++ generator-rb00/app/index.js
@@ -2,8 +2,29 @@
 
 var generators = require('yeoman-generator');
 
+
+
 module.exports = generators.Base.extend({
+    constructor: function () {
+        generators.Base.apply(this, arguments);
+        this.argument('name', { type: String, required: true });
+        this.log('Name: ' + this.name);
+    },
     method1: function() {
         this.log('Hello World of course! What else did you expect!');
     }
 });
+
+/*
+module.exports = generators.NamedBase.extend({
+
+    // Constructor that requires a name
+    constructor: function() {
+        generators.NamedBase.apply(this, arguments);
+
+        this.log('Name: ' + this.name);
+    },
+    method1: function() {
+        this.log('Hello World of course! What else did you expect!');
+    }
+});*/
